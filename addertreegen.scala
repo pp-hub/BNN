575e63e6da999317483557951c95fbeda5f5995b
pp <549204742@qq.com>
2019-12-03 17:16:06 +0800

    update

--- addertreegen.scala
+++ addertreegen.scala
@@ -5,7 +5,9 @@ import chisel3.util._
 
 class AdderTreeGen(num:Int){
    val io = IO(new Bundle{
-    val datain = Input(Vec(num,UInt(4.W)))
-	val dataout = Output()
+    val datain = Input(Vec(2<<num,UInt(4.W)))
+	 val dataout = Output(SInt((4+num).W))
    })
+   val adder =  VecInit(Seq.fill(2<<(num-1))(Module(new SIntFourMFour).io))
+   val 
 }
