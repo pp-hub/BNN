6a5028c25972740c28d717a4cce460ed1feba393
unknown <>
2019-12-03 23:42:25 +0800

    update

--- accumlator.scala
+++ accumlator.scala
@@ -5,6 +5,32 @@ import chisel3.util._
 
 class AccumlatorSet(num:Int){
  val io = IO(new Bundle{
-    val datain = 
+    val datain = Input(SInt(11.W))
+	val dataout = Output(SInt(21.W))
+    val validin = Input(Bool()) 
+    val clear = Input(Bool())	
  })
+   val acc = VecInit(Seq.fill(128)(Module(new Accumlator).io)) 
+   val count = RegInit(0.U(128.W))
+   when(io.validin)
+   {
+    count := count+1.U
+   }
+   val x = for(i <- 0 to 127)
+            yield count === i.U
+  
+   val y = for(i <- 0 until 127)
+            yield 1.U << i
+  
+   val decode = MuxCase(0.U, x zip y)
+   
+   for(i<-0 to 127)
+   {
+    acc(i).io.B := io.datain
+	acc(i).io.CLK := clock
+	acc(i).io.CE := decode(i)
+	acc(i).io.SCLR := decode(i)&&io.clear
+   }
+   io.dataout := acc(count).io.Q
+   io.validout := 
 }
--- addertreegen.scala
+++ addertreegen.scala
@@ -19,41 +19,58 @@ class AdderTreeGen{
    val adder9 =  VecInit(Seq.fill(2)(Module(new SIntNineMNine).io))
    val adder10 =  VecInit(Seq.fill(1)(Module(new SIntTenMTen).io))
    
-   val 
+   val valid = Reg(VecInit(Seq.fill(6)(false.B)))
+   valid(0) := io.valid
+   for(i<-0 to 4)
+   {
+     valid(i+1) := valid(i)
+   }
+   
   for(i<-0 to 63)
   {
     adder4(i).io.A := io.datain(2*i).asSInt
 	addrt4(i).io.B := io.datain(2*1+1).asSInt
-	adder4(i).io.CE := 
+	adder4(i).io.CE := io.valid
+	adder4(i).io.CLK := clock
   }
   for(i<-0 to 31)
   {
     adder5(i).io.A := adder4(2*i).io.S
 	adder5(i).io.B := adder4(2*i+1).io.S
-	adder5(i).io.CE := adder4(2*i).io.
+	adder5(i).io.CE := valid(0)
+	adder5(i).io.CLK := clock
   }
   for(i<-0 to 15)
   {
     adder6(i).io.A := adder5(2*i).io.S
 	adder6(i).io.B := adder5(2*i+1).io.S
-	adder6(i).io.CE := adder5(2*i).io.
+	adder6(i).io.CE := valid(1)
+	adder6(i).io.CLK := clock
   }
   for(i<-0 to 7)
   {
     adder7(i).io.A := adder6(2*i).io.S
 	adder7(i).io.B := adder6(2*i+1).io.S
-	adder7(i).io.CE := adder6(2*i).io.
+	adder7(i).io.CE := valid(2)
+	adder7(i).io.CLK := clock
   }
   for(i<-0 to 3)
   {
     adder8(i).io.A := adder7(2*i).io.S
 	adder8(i).io.B := adder7(2*i+1).io.S
-	adder8(i).io.CE := adder7(2*i).io.
+	adder8(i).io.CE := valid(3)
+	adder8(i).io.CLK := clock
   }
   for(i<-0 to 1)
   {
-    adder9(i).io.A := adder4(2*i).io.S
-	adder9(i).io.B := adder4(2*i+1).io.S
-	adder9(i).io.CE := adder4(2*i).io.
+    adder9(i).io.A := adder8(2*i).io.S
+	adder9(i).io.B := adder8(2*i+1).io.S
+	adder9(i).io.CE := valid(4)
+	adder9(i).io.CLK := clock
   }
+  adder10.io.A := adder9(0).io.S
+  adder10.io.B := adder9(1).io.S
+  adder10.io.CE := valid(5)
+  adder10.io.CLK := clock
+  
 }
