74e71d083f0dce87c7e3699fb30270625b1ea84c
pp <549204742@qq.com>
2019-12-30 13:22:54 +0800

    update

--- information.scala
+++ information.scala
@@ -9,7 +9,7 @@ class InformationToWeightIO extends Bundle{
     val tp = Output(UInt(8.W))
     val floop = Output(UInt(32.W))
     val kloop = Output(UInt(8.W)) 
-    val 
+    val loop = Output(UInt(32.W))
 }
 class InformationToWeightIO extends Bundle{}
 class InformationToConfigIO extends Bundle{
@@ -25,29 +25,33 @@ class Information extends Module{
       
     })
 
-   
-   val mul =  VecInit(Seq.fill(3)(Module(new MUL).io)))
-   mul(0).io.A := io.inToConfig.config(3)
-   mul(0).io.B := io.inToConfig.config(3)
-   mul(0).io.CE := io.fsmToIn.valid
+   val valid1 := RegNext(RegNext(io.fsmToIn.valid))
+   val mul =  VecInit(Seq.fill(4)(Module(new MUL).io)))
+   mul(0).A := io.inToConfig.config(3)
+   mul(0).B := io.inToConfig.config(3)
+   mul(0).CE := io.fsmToIn.valid
    
 
-   mul(1).io.A := io.inToConfig.config(4)
-   mul(1).io.B := io.inToConfig.config(4)
-   mul(1).io.CE := io.fsmToIn.valid 
+   mul(1).A := io.inToConfig.config(4)
+   mul(1).B := io.inToConfig.config(4)
+   mul(1).CE := io.fsmToIn.valid 
 
-   mul(2).io.A := io.inToConfig.config(0)>>io.inToConfig.config(2)
-   mul(2).io.B := io.inToConfig.config(1)>>io.inToConfig.config(2)
-   mul(2).io.CE := io.fsmToIn.valid
+   mul(2).A := io.inToConfig.config(0)>>io.inToConfig.config(2)
+   mul(2).B := io.inToConfig.config(1)>>io.inToConfig.config(2)
+   mul(2).CE := io.fsmToIn.valid
    
-   mul(3).io.A := mul(2).io.P
-   mul(3).io.B := mul(1).io.P
-   mul(3).io.CE := valid1
-
+   mul(3).A := mul(2).P
+   mul(3).B := mul(1).P
+   mul(3).CE := valid1
+   /*
+   mul(4).A := mul(1).P
+   mul(4).B := io.inToConfig.config(0)>>io.inToConfig.config(2)
+   mul(4).CE := valid1
+   */
    io.inToWe.inputC := io.inToConfig.config(0)
    io.inToWe.outputC := io.inToConfig.config(1)
    io.inToWe.tp := io.inToConfig.config(2)
    io.inToWe.floop := mul(0).P 
    io.inToWe.kloop := mul(1).P
-   
+   io.inToWe.loop := mul(3).P
 }
--- mem.scala
+++ mem.scala
@@ -4,3 +4,13 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 
+class WeightToMemIO extends Bundle{
+    val addr = Vec(128,Output(12.W)) 
+}
+class WeightControl extends Module{
+    val io = IO(new Bundle{
+        val inToWe = Flipped(new InformationToWeightIO)
+        val weToMem = new WeightToMemIO
+    })
+    
+}
