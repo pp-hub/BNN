1156f5693dd8c8c4801055ada1c2c7df2a4a4b4c
pp <549204742@qq.com>
2019-12-31 16:41:07 +0800

    hahah

--- mem.scala
+++ mem.scala
@@ -12,5 +12,12 @@ class WeightControl extends Module{
         val inToWe = Flipped(new InformationToWeightIO)
         val weToMem = new WeightToMemIO
     })
+    val addr = RegInit(0.U(12.W))
+    val count = RegInit(0.U(32.W))
+    addr := Mux(count < io.inToWe.loop,addr+1.U,0.U)
     
+    for(i<-0 to 127)
+    {
+     io.weToMem.addr(i) := addr
+    }
 }
